dcc3ee8508c1bcc125a1bedc88e1eac3044c618c
Santo Pfingsten <core@lusito.info>
2020-11-26 15:04:01 +0100

    upstream body_type test

--- packages/testbed/src/tests/core/body_types.ts
+++ packages/testbed/src/tests/core/body_types.ts
@@ -41,84 +41,78 @@ class BodyTypes extends Test {
         super();
 
         const ground = this.m_world.CreateBody();
+        {
+            const shape = new b2EdgeShape();
+            shape.SetTwoSided(new b2Vec2(-20, 0), new b2Vec2(20, 0));
 
-        /* b2EdgeShape */
-        const gshape = new b2EdgeShape();
-        gshape.SetTwoSided(new b2Vec2(-20.0, 0.0), new b2Vec2(20.0, 0.0));
-
-        ground.CreateFixture({ shape: gshape });
+            ground.CreateFixture({ shape });
+        }
 
         // Define attachment
         {
             this.m_attachment = this.m_world.CreateBody({
                 type: b2BodyType.b2_dynamicBody,
                 position: {
-                    x: 0.0,
-                    y: 3.0,
+                    x: 0,
+                    y: 3,
                 },
             });
 
-            /* b2PolygonShape */
             const shape = new b2PolygonShape();
-            shape.SetAsBox(0.5, 2.0);
-            this.m_attachment.CreateFixture({ shape, density: 2.0 });
+            shape.SetAsBox(0.5, 2);
+            this.m_attachment.CreateFixture({ shape, density: 2 });
         }
 
         // Define platform
         {
             this.m_platform = this.m_world.CreateBody({
                 type: b2BodyType.b2_dynamicBody,
-                position: { x: -4.0, y: 5.0 },
+                position: { x: -4, y: 5 },
             });
 
-            /* b2PolygonShape */
             const shape = new b2PolygonShape();
-            shape.SetAsBox(0.5, 4.0, new b2Vec2(4.0, 0.0), 0.5 * Math.PI);
+            shape.SetAsBox(0.5, 4, new b2Vec2(4, 0), 0.5 * Math.PI);
 
             this.m_platform.CreateFixture({
                 shape,
                 friction: 0.6,
-                density: 2.0,
+                density: 2,
             });
 
-            /* b2RevoluteJointDef */
             const rjd = new b2RevoluteJointDef();
-            rjd.Initialize(this.m_attachment, this.m_platform, new b2Vec2(0.0, 5.0));
-            rjd.maxMotorTorque = 50.0;
+            rjd.Initialize(this.m_attachment, this.m_platform, new b2Vec2(0, 5));
+            rjd.maxMotorTorque = 50;
             rjd.enableMotor = true;
             this.m_world.CreateJoint(rjd);
 
-            /* b2PrismaticJointDef */
             const pjd = new b2PrismaticJointDef();
-            pjd.Initialize(ground, this.m_platform, new b2Vec2(0.0, 5.0), new b2Vec2(1.0, 0.0));
+            pjd.Initialize(ground, this.m_platform, new b2Vec2(0, 5), new b2Vec2(1, 0));
 
-            pjd.maxMotorForce = 1000.0;
+            pjd.maxMotorForce = 1000;
             pjd.enableMotor = true;
-            pjd.lowerTranslation = -10.0;
-            pjd.upperTranslation = 10.0;
+            pjd.lowerTranslation = -10;
+            pjd.upperTranslation = 10;
             pjd.enableLimit = true;
 
             this.m_world.CreateJoint(pjd);
 
-            this.m_speed = 3.0;
+            this.m_speed = 3;
         }
 
         // Create a payload
         {
-            /* b2Body */
             const body = this.m_world.CreateBody({
                 type: b2BodyType.b2_dynamicBody,
-                position: { x: 0.0, y: 8.0 },
+                position: { x: 0, y: 8 },
             });
 
-            /* b2PolygonShape */
             const shape = new b2PolygonShape();
             shape.SetAsBox(0.75, 0.75);
 
             body.CreateFixture({
                 shape,
                 friction: 0.6,
-                density: 2.0,
+                density: 2,
             });
         }
     }
@@ -129,8 +123,8 @@ class BodyTypes extends Test {
             hotKeyPress("s", "Set Static Body", () => this.m_platform.SetType(b2BodyType.b2_staticBody)),
             hotKeyPress("k", "Set Kinematic Body", () => {
                 this.m_platform.SetType(b2BodyType.b2_kinematicBody);
-                this.m_platform.SetLinearVelocity(new b2Vec2(-this.m_speed, 0.0));
-                this.m_platform.SetAngularVelocity(0.0);
+                this.m_platform.SetLinearVelocity(new b2Vec2(-this.m_speed, 0));
+                this.m_platform.SetAngularVelocity(0);
             }),
         ];
     }
@@ -138,12 +132,10 @@ class BodyTypes extends Test {
     public Step(settings: Settings, timeStep: number): void {
         // Drive the kinematic body.
         if (this.m_platform.GetType() === b2BodyType.b2_kinematicBody) {
-            /* b2Vec2 */
             const { p } = this.m_platform.GetTransform();
-            /* b2Vec2 */
             const v = this.m_platform.GetLinearVelocity();
 
-            if ((p.x < -10.0 && v.x < 0.0) || (p.x > 10.0 && v.x > 0.0)) {
+            if ((p.x < -10 && v.x < 0) || (p.x > 10 && v.x > 0)) {
                 this.m_platform.SetLinearVelocity(new b2Vec2(-v.x, v.y));
             }
         }
