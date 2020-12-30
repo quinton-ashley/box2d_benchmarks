595e63fda2c391f048c87cc5008f83e6a6c6c3c1
Santo Pfingsten <core@lusito.info>
2020-12-30 18:17:48 +0100

    further comment cleanup

--- packages/core/src/dynamics/b2_contact_solver.ts
+++ packages/core/src/dynamics/b2_contact_solver.ts
@@ -1,20 +1,24 @@
-/*
- * Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
- *
- * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 1. The origin of this software must not be misrepresented; you must not
- * claim that you wrote the original software. If you use this software
- * in a product, an acknowledgment in the product documentation would be
- * appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- * misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- */
+// MIT License
+
+// Copyright (c) 2019 Erin Catto
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 // DEBUG: import { b2Assert } from "../common/b2_common";
 import {
@@ -29,9 +33,6 @@ import { b2Clamp, b2Vec2, b2Mat22, b2Rot, b2Transform } from "../common/b2_math"
 import { b2WorldManifold, b2ManifoldType } from "../collision/b2_collision";
 import { b2Contact } from "./b2_contact";
 import { b2TimeStep, b2Position, b2Velocity } from "./b2_time_step";
-
-// Solver debugging is normally disabled because the block solver sometimes has to deal with a poorly conditioned effective mass matrix.
-// #define B2_DEBUG_SOLVER 0
 
 let g_blockSolve = true;
 
@@ -641,10 +642,6 @@ export class b2ContactSolver {
                 // Compute b'
                 b.Subtract(b2Mat22.MultiplyVec2(vc.K, a, b2Vec2.s_t0));
 
-                // #if B2_DEBUG_SOLVER === 1
-                // const k_errorTol = 1e-3f;
-                // #endif
-
                 for (;;) {
                     //
                     // Case 1: vn = 0
@@ -676,19 +673,6 @@ export class b2ContactSolver {
                         // Accumulate
                         cp1.normalImpulse = x.x;
                         cp2.normalImpulse = x.y;
-
-                        // #if B2_DEBUG_SOLVER === 1
-                        // // Postconditions
-                        // dv1 = vB + b2Cross(wB, cp1->rB) - vA - b2Cross(wA, cp1->rA);
-                        // dv2 = vB + b2Cross(wB, cp2->rB) - vA - b2Cross(wA, cp2->rA);
-
-                        // // Compute normal velocity
-                        // vn1 = b2Dot(dv1, normal);
-                        // vn2 = b2Dot(dv2, normal);
-
-                        // b2Assert(Math.abs(vn1 - cp1->velocityBias) < k_errorTol);
-                        // b2Assert(Math.abs(vn2 - cp2->velocityBias) < k_errorTol);
-                        // #endif
                         break;
                     }
 
@@ -721,16 +705,6 @@ export class b2ContactSolver {
                         // Accumulate
                         cp1.normalImpulse = x.x;
                         cp2.normalImpulse = x.y;
-
-                        // #if B2_DEBUG_SOLVER === 1
-                        // // Postconditions
-                        // dv1 = vB + b2Cross(wB, cp1->rB) - vA - b2Cross(wA, cp1->rA);
-
-                        // // Compute normal velocity
-                        // vn1 = b2Dot(dv1, normal);
-
-                        // b2Assert(Math.abs(vn1 - cp1->velocityBias) < k_errorTol);
-                        // #endif
                         break;
                     }
 
@@ -762,18 +736,6 @@ export class b2ContactSolver {
                         // Accumulate
                         cp1.normalImpulse = x.x;
                         cp2.normalImpulse = x.y;
-
-                        /*
-            #if B2_DEBUG_SOLVER === 1
-            // Postconditions
-            dv2 = vB + b2Cross(wB, cp2->rB) - vA - b2Cross(wA, cp2->rA);
-
-            // Compute normal velocity
-            vn2 = b2Dot(dv2, normal);
-
-            b2Assert(Math.abs(vn2 - cp2->velocityBias) < k_errorTol);
-            #endif
-            */
                         break;
                     }
 
--- packages/core/src/dynamics/b2_island.ts
+++ packages/core/src/dynamics/b2_island.ts
@@ -1,20 +1,24 @@
-/*
- * Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
- *
- * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 1. The origin of this software must not be misrepresented; you must not
- * claim that you wrote the original software. If you use this software
- * in a product, an acknowledgment in the product documentation would be
- * appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- * misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- */
+// MIT License
+
+// Copyright (c) 2019 Erin Catto
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 // DEBUG: import { b2Assert } from "../common/b2_common";
 import {
@@ -441,39 +445,6 @@ export class b2Island {
                 break;
             }
         }
-
-        /*
-  #if 0
-    // Is the new position really safe?
-    for (int32 i = 0; i < this.m_contactCount; ++i) {
-      b2Contact* c = this.m_contacts[i];
-      b2Fixture* fA = c.GetFixtureA();
-      b2Fixture* fB = c.GetFixtureB();
-
-      b2Body* bA = fA.GetBody();
-      b2Body* bB = fB.GetBody();
-
-      int32 indexA = c.GetChildIndexA();
-      int32 indexB = c.GetChildIndexB();
-
-      b2DistanceInput input;
-      input.proxyA.Set(fA.GetShape(), indexA);
-      input.proxyB.Set(fB.GetShape(), indexB);
-      input.transformA = bA.GetTransform();
-      input.transformB = bB.GetTransform();
-      input.useRadii = false;
-
-      b2DistanceOutput output;
-      b2SimplexCache cache;
-      cache.count = 0;
-      b2Distance(&output, &cache, &input);
-
-      if (output.distance === 0 || cache.count === 3) {
-        cache.count += 0;
-      }
-    }
-  #endif
-  */
 
         // Leap of faith to new safe state.
         this.m_bodies[toiIndexA].m_sweep.c0.Copy(this.m_positions[toiIndexA].c);
